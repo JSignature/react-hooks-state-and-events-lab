16a50e97b60f46a4221efb541354a8abbfed0128
Jimmy Andreas <jimmysignature@gmail.com>
2022-06-02 21:44:12 -0400

    Added mode

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,30 @@
-import React from "react";
-import ShoppingList from "./ShoppingList";
-import itemData from "../data/items";
+import React, { useState } from 'react'
+import ShoppingList from './ShoppingList'
+import itemData from '../data/items'
 
 function App() {
-
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
-
+  // const appClass = false ? "App dark" : "App light"
+  const [mode, setMode] = useState('App light')
+  function changeMode() {
+    if (mode === 'App light') {
+      setMode('App dark')
+    } else {
+      setMode('App light')
+    }
+  }
   return (
-    <div className={appClass}>
+    <div className={mode}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={changeMode}>
+          {mode === 'App light' ? 'Dark Mode' : 'Light Mode'}
+        </button>
       </header>
       <ShoppingList items={itemData} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
